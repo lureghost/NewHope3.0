ba95df09006818927b01585d32f45b208b49bdcc
Lenovo <Lenovo@BLUE-ROSE>
2020-04-07 12:51:03 +0800

    添加项目文件。

---
+++ NewHope/Controllers/NewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewHope.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult test() {
+
+            return View();
+        }
+    }
+}
