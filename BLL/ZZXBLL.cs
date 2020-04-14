746df85ba8e654dcb5a91172236c747ca21aa2f4
zzx <zzx@DESKTOP-35EQD4T>
2020-04-14 14:34:32 +0800

    标识符管理页面

---
+++ BLL/ZZXBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MOD;
+using DAL;
+namespace BLL
+{
+   public class ZZXBLL
+    {
+        #region 用户表操作(Admin)
+        public static IQueryable AdminChaXun()
+        {
+            return ZZXDAL.AdminChaXun();
+        }
+        #endregion
+        }
+}
---
+++ DAL/ZZXDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MOD;
+namespace DAL
+{
+   public class ZZXDAL
+    {
+        #region 用户表操作(Admin)
+        public static IQueryable AdminChaXun()
+        {
+            NewHope4Entities ent = new NewHope4Entities();
+            var obj = from p in ent.Admin
+                      select new
+                      {
+                          Did = p.Did,
+                          Email = p.Email,
+                          LoginNum = p.LoginNum,
+                          UserID = p.UserID,
+                          UserName = p.UserName,
+                          Name = p.Name,
+                          Phone = p.Phone,
+                          RID = p.RID,
+                          Userpwd = p.Userpwd,
+                          UserTypeID = p.UserTypeID,
+                          state = p.state,
+                      };
+            return obj;
+        }
+        #endregion
+    }
+}
