2f4f6448c05ddb6a65aa4f5ac5799a625690694b
1 <1@DESKTOP-MJ026DI>
2020-04-14 10:49:38 +0800

    yq_14_1049

--- NewHope/Controllers/YQController.cs
+++ NewHope/Controllers/YQController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Models;
+using BLL;
 namespace NewHope.Controllers
 {
     public class YQController : Controller
